8ea9a8a89b77c563adf343af610784003eeb4d01
RY022 <r-sekioka@seassist.co.jp>
2025-03-03 01:05:02 +0900

    first commit

---
+++ src/main/java/com/example/demo/WebSecurityConfig.java
@@ -0,0 +1,24 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableWebSecurity
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter{
+	 @Override
+	    protected void configure(HttpSecurity http) throws Exception {
+	        http
+	            .authorizeRequests()
+	            	.antMatchers("/menupage").permitAll()
+	                .anyRequest().authenticated()
+	                .and()
+	                .formLogin()
+	                	.loginPage("/loginpage")
+	                	.loginProcessingUrl("file:///C:/pleiades/2024-09/workspace/Attendance_project/src/main/project/html/loginpage.html")
+	                	.usernameParameter("userID")
+	                	.passwordParameter("password")
+	                	.defaultSuccessUrl("file:///C:/pleiades/2024-09/workspace/Attendance_project/src/main/project/menupage.html")
+	                	.failureUrl("/menupage");
+	    }
+
+}
